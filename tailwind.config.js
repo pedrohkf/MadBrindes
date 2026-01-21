78e198a72a94577eb2d20c0dc6678edc39b204fe
pedrohkf <phkfaria@gmail.com>
2026-01-21 14:42:43 -0300

    feat: add config and tailwind in project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+}
